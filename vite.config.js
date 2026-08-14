eb174fe8fd7576fbe4e3af11bd671303758e990a
shambhawijhaa-cyber <shambhawijhaa@gmail.com>
2026-08-14 13:40:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs@plugin-react'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mysite/', // 👈 MUST MATCH YOUR REPO NAME EXACTLY
+  base: './', // 👈 Relative base path fixes blank screen on GitHub Pages
 })
